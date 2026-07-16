b797a5afe8218ed5766954300f06ce3c8fdf62a5
aliyun5895768908 <514974922@qq.com>
2026-07-16 08:52:06 +0800

    remove ignored files from tracking

--- frontend/sub-nlp/node_modules/vite-plugin-qiankun/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { PluginOption } from 'vite';
-export declare type MicroOption = {
-    useDevMode?: boolean;
-};
-declare type PluginFn = (qiankunName: string, microOption?: MicroOption) => PluginOption;
-declare const htmlPlugin: PluginFn;
-export default htmlPlugin;
